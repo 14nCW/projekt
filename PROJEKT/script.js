7b258fb3e90d98dcc84286935e097b225b80d66e
Ian Canals <canals.ian@gmail.com>
2021-06-10 11:17:30 +0200

    poprawione

--- PROJEKT/script.js
+++ PROJEKT/script.js
@@ -1,6 +1,4 @@
-
 function togglePopup() {
     document.getElementById("popup-1")
         .classList.toggle("active")
 }
-
---
+++ PROJEKT/signUp.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['email']) && isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['password'])) {
+    $dbConnection = mysqli_connect("localhost", "ian", "123", "projekt");
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $password = $_POST['password'];
+
+    $query = "INSERT INTO signup(email, name, surname, password) VALUES (?, ?, ?, ?)";
+    $statement = mysqli_prepare($dbConnection, $query);
+    mysqli_stmt_bind_param($statement, "ssss", $email, $name, $surname, $password);
+    if (!$result = mysqli_stmt_execute($statement)) {
+        echo mysqli_error($dbConnection);
+    }
+    mysqli_close($dbConnection);
+    echo("hello");
+    exit();
+}
