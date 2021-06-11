0cce79f652a287ac68d77ad804d6048ed8035332
Ian Canals <canals.ian@gmail.com>
2021-06-11 12:03:21 +0200

    2021/06/11 12,03

--- PROJEKT/script.js
+++ PROJEKT/script.js
@@ -1,7 +1,44 @@
+//buttons
+document.getElementById("first-button").onclick = togglePopup;
+
+// variables
+let gamestatus = false;
+let turn = true;
+
+//HTML
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelectorAll('.game-cell');
+const statusDiv = document.querySelector('.turns');
+
+// functions
 function togglePopup() {
-    document.getElementById("popup-1")
-        .classList.toggle("active")
+    document.getElementById("popup-1").classList.toggle("active");
 }
 
-let gamestatus = false;
-let
+const tiles = () => {
+    const topLeft = cellDivs[0].classList[1];
+    const topMiddle = cellDivs[1].classList[1];
+    const topRight = cellDivs[2].classList[1];
+    const middleLeft = cellDivs[3].classList[1];
+    const middleMidle = cellDivs[4].classList[1];
+    const middleRight = cellDivs[5].classList[1];
+    const bottomLeft = cellDivs[6].classList[1];
+    const bottomMiddle = cellDivs[7].classList[1];
+    const bottomRight = cellDivs[8].classList[1];
+}
+
+const CellClick = (e) => {
+    const classList = r.target.classList;
+
+    if (!gamestatus || classList[1] === 'x' || classList[1] === 'o') {
+        return;
+    }
+
+    if (turn) {
+        classList.add('x');
+        tiles();
+    } else {
+        classList.add('o');
+        tiles();
+    }
+}
